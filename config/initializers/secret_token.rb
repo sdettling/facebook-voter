80c5be1a8a86cde999454fd0d4560b983d85abbd
Stephen Dettling <sdettling@gmail.com>
2012-01-13 17:30:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookVoter::Application.config.secret_token = 'f2a9a7c88a35ac948a2127532a5baec832cd6f9fbeb9778cc866b004e5abf7eafaf54b1b90f3504c77fbed595393ea626d2ff279795b6c9d8eca5b55952973ea'
