09f145d5f2baf8b5a2a24631248f25fcb7dc0f07
Stephen Dettling <sdettling@gmail.com>
2014-02-05 22:40:11 -0500

    debug

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -523,4 +523,11 @@ $(document).ready(function() {
       saveVotes();
     }
   }
+  (function(d){
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+  }(document));
 });
