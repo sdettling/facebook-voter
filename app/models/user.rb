39742a76b3f0f9a8a792fc9825f9dde950433473
Stephen Dettling <sdettling@gmail.com>
2014-02-06 00:01:54 -0500

    dedupe ?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   validates :fbid, uniqueness: true
+  def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|model| [model.fbid] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
 end
+User.dedupe
